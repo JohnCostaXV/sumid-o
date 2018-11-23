ec58f5df137d013b206455eb0a3bc1dc95b073df
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-11-23 15:20:58 -0200

    Update main.py

--- main.py
+++ main.py
@@ -268,25 +268,28 @@ async def on_message(message):
                                                                         await msg.add_reaction(":correto:515523764297924618")
                                                                         await msg.add_reaction(":incorreto:515523818358571039")
                                                                         def opt_check(reaction, user):
-                                                                            return reaction.message.id == msg.id and user == message.author and str(reaction.emoji) in '<:correto:515523764297924618>', '<:incorreto:515523818358571039>' 
+                                                                            return reaction.message.id == msg.id and user == reaction.author and str(reaction.emoji) in '<:correto:515523764297924618>', '<:incorreto:515523818358571039>' 
                                                                             
                                                                         try:
                                                                             reaction, user = await client.wait_for("reaction_add", check=opt_check, timeout=172800)
                                                                         
                                                                             if str(reaction.emoji) == '<:correto:515523764297924618>':
+                                                                                await msg.delete()
                                                                                 await logs.send(f"<:correto:510894022861127680> | {message.author.mention}, seu bot `{usuario}` foi **aceito** pelo **{user.name}**.")
                                                                                 await author.send(f"<:correto:510894022861127680> | O seu bot `{usuario}` foi **aceito** pelo **{user.name}**;")
 
                                                                             elif str(reaction.emoji) == '<:incorreto:515523818358571039>':
                                                                                 mtv1 = await client.get_channel(507570211499671576).send(f"{user.mention}, diga o motivo para **recusar** o bot `{usuario}`: `(2 minutos)`")
                                                                                 try:
-                                                                                    mtv = await client.wait_for('message', check=lambda message: user == user, timeout=120)
+                                                                                    mtv = await client.wait_for('message', check=lambda message: user == reaction.author, timeout=120)
                                                                                     if mtv.content == mtv.content:
                                                                                         await mtv1.delete()
+                                                                                        await msg.delete()
                                                                                         await mtv.delete()
                                                                                         await logs.send(f"<:incorreto:510894050103263245> | {message.author.mention}, seu bot `{usuario}` foi **recusado** pelo **{user.name}**.\n\nMotivo:```{mtv.content}```")
                                                                                         await author.send(f"<:incorreto:510894050103263245> | O seu bot `{usuario}` foi **recusado** pelo **{user.name}**.\n\nMotivo:```{mtv.content}```")
                                                                                 except asyncio.TimeoutError:
+                                                                                    await msg.delete()
                                                                                     await mtv1.delete()
                                                                                     await logs.send(f"<:incorreto:510894050103263245> | {message.author.mention}, seu bot `{usuario}` foi **recusado** pelo **{user.name}**.\n\nMotivo:```Nenhum motivo informado```")
                                                                                     await author.send(f"<:incorreto:510894050103263245> | O seu bot `{usuario}` foi **recusado** pelo **{user.name}**.\n\nMotivo:```Nenhum motivo informado```")
@@ -340,26 +343,29 @@ async def on_message(message):
                                                                 await msg.add_reaction(":correto:515523764297924618")
                                                                 await msg.add_reaction(":incorreto:515523818358571039")
                                                                 def opt_check(reaction, user):
-                                                                    return reaction.message.id == msg.id and user == message.author and str(reaction.emoji) in '<:correto:515523764297924618>', '<:incorreto:515523818358571039>' 
+                                                                    return reaction.message.id == msg.id and user == reaction.author and str(reaction.emoji) in '<:correto:515523764297924618>', '<:incorreto:515523818358571039>' 
                                                                             
                                                                 try:
                                                                     reaction, user = await client.wait_for("reaction_add", check=opt_check, timeout=172800)
                                                                         
                                                                     if str(reaction.emoji) == '<:correto:515523764297924618>':
+                                                                        await msg.delete()
                                                                         await logs.send(f"<:correto:510894022861127680> | {message.author.mention}, seu bot `{usuario}` foi **aceito** pelo **{user.name}**.")
                                                                         await author.send(f"<:correto:510894022861127680> | O seu bot `{usuario}` foi **aceito** pelo **{user.name}**;")
 
                                                                     elif str(reaction.emoji) == '<:incorreto:515523818358571039>':
                                                                         mtv1 = await client.get_channel(507570211499671576).send(f"{user.mention}, diga o motivo para **recusar** o bot `{usuario}`: `(2 minutos)`")
                                                                         try:
-                                                                            mtv = await client.wait_for('message', check=lambda message: user == user, timeout=120)
+                                                                            mtv = await client.wait_for('message', check=lambda message: user == reaction.author, timeout=120)
                                                                             if mtv.content == mtv.content:
                                                                                 await mtv1.delete()
                                                                                 await mtv.delete()
+                                                                                await msg.delete()
                                                                                 await logs.send(f"<:incorreto:510894050103263245> | {message.author.mention}, seu bot `{usuario}` foi **recusado** pelo **{user.name}**.\n\nMotivo:```{mtv.content}```")
                                                                                 await author.send(f"<:incorreto:510894050103263245> | O seu bot `{usuario}` foi **recusado** pelo **{user.name}**.\n\nMotivo:```{mtv.content}```")
                                                                         except asyncio.TimeoutError:
                                                                             await mtv1.delete()
+                                                                            await msg.delete()
                                                                             await logs.send(f"<:incorreto:510894050103263245> | {message.author.mention}, seu bot `{usuario}` foi **recusado** pelo **{user.name}**.\n\nMotivo:```Nenhum motivo informado```")
                                                                             await author.send(f"<:incorreto:510894050103263245> | O seu bot `{usuario}` foi **recusado** pelo **{user.name}**.\n\nMotivo:```Nenhum motivo informado```")
 
